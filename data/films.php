85977a6fd0b6a67c08d4905e2e6ebdffb4501e51
Erik <erikdmitchell@gmail.com>
2018-12-08 13:29:46 -0600

    added support to insert a featured image

--- data/films.php
+++ data/films.php
@@ -222,7 +222,9 @@ function bond_api_install_films($actors, $directors) {
         update_post_meta($post_id, '_director_id', $director_id);                  
 
         // set featured image.
-        //bond_api_install_insert_image($film_data['image'], $post_id);        
+        if (!has_post_thumbnail($post_id)) :
+            bond_api_install_insert_image($film_data['image'], $post_id);        
+        endif;
     endforeach;
 
     return $films;
@@ -237,38 +239,40 @@ function bond_api_install_films($actors, $directors) {
  * @return void
  */
 function bond_api_install_insert_image($image, $post_id) {
-        // load featured image. -- TURN INTO FUNCTION
-        $image = $film_data['image'];
-        
-        // magic sideload image returns an HTML image, not an ID
-        $media = media_sideload_image($image, $post_id);        
+    // required libraries for media_handle_sideload
+    require_once(ABSPATH . 'wp-admin/includes/file.php');
+    require_once(ABSPATH . 'wp-admin/includes/media.php');
+    require_once(ABSPATH . 'wp-admin/includes/image.php');
+    
+    // magic sideload image returns an HTML image, not an ID
+    $media = media_sideload_image($image, $post_id);        
 
-        // therefore we must find it so we can set it as featured ID
-        if (!empty($media) && !is_wp_error($media)) :
-            $args = array(
-                'post_type' => 'attachment',
-                'posts_per_page' => -1,
-                'post_status' => 'any',
-                'post_parent' => $post_id
-            );
-        
-            // reference new image to set as featured
-            $attachments = get_posts($args);
-        
-            if (isset($attachments) && is_array($attachments)) :
-                foreach($attachments as $attachment) :
-                    // grab source of full size images (so no 300x150 nonsense in path)
-                    $image = wp_get_attachment_image_src($attachment->ID, 'full');
+    // therefore we must find it so we can set it as featured ID
+    if (!empty($media) && !is_wp_error($media)) :
+        $args = array(
+            'post_type' => 'attachment',
+            'posts_per_page' => -1,
+            'post_status' => 'any',
+            'post_parent' => $post_id
+        );
+    
+        // reference new image to set as featured
+        $attachments = get_posts($args);
+    
+        if (isset($attachments) && is_array($attachments)) :
+            foreach($attachments as $attachment) :
+                // grab source of full size images
+                $image = wp_get_attachment_image_src($attachment->ID, 'full');
+                
+                // determine if in the $media image we created, the string of the URL exists
+                if (strpos($media, $image[0]) !== false) :
+                    // if so, we found our image. set it as thumbnail
+                    set_post_thumbnail($post_id, $attachment->ID);
                     
-                    // determine if in the $media image we created, the string of the URL exists
-                    if (strpos($media, $image[0]) !== false) :
-                        // if so, we found our image. set it as thumbnail
-                        set_post_thumbnail($post_id, $attachment->ID);
-                        
-                        // only want one image
-                        break;
-                    endif;
-                endforeach;
-            endif;
-        endif;    
+                    // only want one image
+                    break;
+                endif;
+            endforeach;
+        endif;
+    endif;    
 }
