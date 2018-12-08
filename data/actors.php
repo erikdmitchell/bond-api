5f87192d58c7d46519c6ac08aed7b47af48f2afe
Erik <erikdmitchell@gmail.com>
2018-12-08 10:32:07 -0600

    added actors data

--- data/actors.php
+++ data/actors.php
@@ -1,15 +1,21 @@
-
-PHPCBF RESULT SUMMARY
-----------------------------------------------------------------------
-FILE                                                  FIXED  REMAINING
-----------------------------------------------------------------------
-...gins/bond-api/includes/class-bond-api-install.php  8      3
-...blic_html/wp-content/plugins/bond-api/gulpfile.js  243    30
-...c_html/wp-content/plugins/bond-api/data/films.php  125    2
-----------------------------------------------------------------------
-A TOTAL OF 376 ERRORS WERE FIXED IN 3 FILES
-----------------------------------------------------------------------
-
-Time: 2.46 secs; Memory: 14Mb
-
-
+<?php
+$actors = array(
+    array(
+        'title' => 'Sean Connery',
+    ),
+    array(
+        'title' => 'George Lazenby',
+    ),
+    array(
+        'title' => 'Roger Moore',
+    ),
+    array(
+        'title' => 'Timothy Dalton',
+    ),
+    array(
+        'title' => 'Pierce Brosnan',
+    ),                        
+    array(
+        'title' => 'Daniel Craig',
+    ),    
+);
