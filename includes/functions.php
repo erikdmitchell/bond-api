712c0ef71e9cc0dcd543682333f1141f8ff834ea
Erik <erikdmitchell@gmail.com>
2018-12-09 10:13:46 -0600

    added directors metabox (films)

--- includes/functions.php
+++ includes/functions.php
@@ -7,3 +7,11 @@ function bond_api_get_actors_films($actor_id = 0) {
 
     return $films;    
 }
+
+function bond_api_get_directors_films($director_id = 0) {
+    global $wpdb;
+    
+    $films = $wpdb->get_col("SELECT post_id FROM $wpdb->postmeta WHERE meta_value = $director_id");
+
+    return $films;    
+}
